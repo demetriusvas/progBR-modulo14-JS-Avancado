860a82a0f8c2a0d503ac8fb14eeef5b924f4d612
Demetrius Vignati <demetriusvas@gmail.com>
2022-05-10 17:34:18 -0300

    atualização do Filter

--- aula08_Filter/script.js
+++ aula08_Filter/script.js
@@ -10,19 +10,8 @@ let alunos = [
     novoAluno("Joao", 35)
 ];
 
-let jogadores = [
-    novoAluno("Igor", 23),
-    novoAluno("Leo", 45),
-    novoAluno("Michelle", 29),
-    novoAluno("Maria", 35)
-]
-
 function temMenosDe30(aluno){
     return aluno.idade < 30;
-}
-
-function temMaisDe30(aluno){
-    return aluno.idade > 30;
 }
 /*
 let alunosComMenosDe30 = alunos.filter(temMaisDe30);
@@ -30,4 +19,4 @@ let alunosComMenosDe30 = alunos.filter(temMaisDe30);
 console.log(alunosComMenosDe30);
 */
 
-console.log(jogadores.filter(temMaisDe30));
+console.log(alunos.filter(temMenosDe30));
